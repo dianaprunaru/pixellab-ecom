66ca18f3ccd8bfb43ea81f625b0775813f6c78af
dianaprunaru <mosoianudiana@gmail.com>
2022-09-26 19:57:02 +0300

    Initial commit

---
+++ pages/contact.jsx
@@ -0,0 +1,11 @@
+import Head from "next/head";
+
+const ContactPage = () => {
+  return (
+    <>
+      <Head></Head>
+    </>
+  );
+};
+export default ContactPage;
+// it's mandatory to return a default export for every page
