269c7e0d2ef2829fdcef93577dc67f65aa11941d
dianaprunaru <mosoianudiana@gmail.com>
2022-09-26 20:59:36 +0300

    intermediary commit

---
+++ src/pages/index.jsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+const Home = () => {
+  return (
+    <>
+      <Head>
+        <title>Homepage</title>
+      </Head>
+
+      <main>homepage</main>
+    </>
+  );
+};
+
+export default Home;
