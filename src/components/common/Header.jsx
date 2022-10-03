ef6182ec1b8d824ff3e088287eaaef14e986509d
dianaprunaru <mosoianudiana@gmail.com>
2022-10-03 18:35:37 +0300

    Tema 28sept

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 import { useState } from 'react';
 import { CgMenu } from 'react-icons/cg';
+import { CgClose } from 'react-icons/cg';
 import { GiAquarium } from 'react-icons/gi';
 
 export const Header = () => {
@@ -8,7 +9,7 @@ export const Header = () => {
 
   return (
     <>
-      <section className="flex items-center h-full relative justify-between z-10">
+      <section className="flex lg:flex-col lg:h-1/2 items-center py-4 relative justify-between z-10">
         <Link href="/">
           <a title="Home">
             <GiAquarium size="32"></GiAquarium>
@@ -22,7 +23,11 @@ export const Header = () => {
             setMenuOpen(!menuOpen);
           }}
         >
-          <CgMenu size="32"></CgMenu>
+          {menuOpen ? (
+            <CgClose className="flex lg:justify-center" size="32"></CgClose>
+          ) : (
+            <CgMenu size="32"></CgMenu>
+          )}
         </button>
       </section>
 
@@ -30,21 +35,28 @@ export const Header = () => {
         <nav
           className={`absolute ${
             menuOpen ? 'top-0' : '-top-full'
-          } left-0 h-screen w-screen text-white bg-neutral-900`}
+          } left-0 h-screen w-screen text-white bg-neutral-900 `}
         >
-          <ul>
-            <li>
-              <Link href="/">
-                <a title="Home">Home</a>
-              </Link>
-            </li>
+          <div>
+            <ul>
+              <li>
+                <Link href="/">
+                  <a href="Shop">Shop</a>
+                </Link>
+              </li>
+              <li>
+                <Link href="/">
+                  <a title="Home">Home</a>
+                </Link>
+              </li>
 
-            <li>
-              <Link href="/">
-                <a title="Contact">Contact</a>
-              </Link>
-            </li>
-          </ul>
+              <li>
+                <Link href="/">
+                  <a title="Contact">Contact</a>
+                </Link>
+              </li>
+            </ul>
+          </div>
         </nav>
       ) : (
         <></>
