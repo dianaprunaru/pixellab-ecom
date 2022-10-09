b85fb7581ffc7696afd1c9199e437e47f18d5469
dianaprunaru <mosoianudiana@gmail.com>
2022-10-09 18:15:28 +0300

    9oct_homework feedback

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -2,29 +2,26 @@ import { css } from '@emotion/css';
 import { IoIosArrowDropup } from 'react-icons/io';
 
 export const Footer = () => {
-  const scrollToTop = () => {
-    document.getElementById('scroller').scroll(0, 0);
-  };
-
   // css`` not working
   const hrRuleCss = css`
     width: 100%;
     position: relative;
     margin-top: 15px;
     margin-bottom: 15px;
-    border-bottom: 0.5px solid #d7d7d7;
+    border-bottom: 1px solid #d7d7d7;
   `;
 
   // css`` not working
   const hrIcon = css`
     position: relative;
+    background-color: transparent;
     top: 3px;
     color: #d7d7d7;
   `;
 
   return (
     <>
-      <section className="container px-4 mt-24 lg:px-0 mx-auto py-24 text-center lg:text-left grid lg:grid-cols-4 gap-9 font-['Work_Sans]">
+      <section className="container px-4 mt-24 lg:px-0 mx-auto py-24 text-center lg:text-left grid lg:grid-cols-4 gap-9 font-['Work_Sans']">
         <div className="">
           <h1 className="uppercase py-2 font-semibold">Los Angeles</h1>
           <ul>
@@ -74,23 +71,27 @@ export const Footer = () => {
         </div>
       </section>
 
-      <div className="flex">
+      <div className="flex container">
         <div className={hrRuleCss}></div>
         <div className={hrIcon}>
           <button
             type="button"
             id="scroller"
             title="Back to top"
-            onClick={scrollToTop}
+            onClick={() => {
+              window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
+            }}
           >
-            <IoIosArrowDropup size="32"></IoIosArrowDropup>
+            <IoIosArrowDropup size="24"></IoIosArrowDropup>
           </button>
         </div>
         <div className={hrRuleCss}></div>
       </div>
 
-      <section className="container px-4 lg:px-0 mx-auto">
-        This is your bottom
+      <section className="container px-4 lg:px-0 mx-auto flex justify-center">
+        <h2 className="font-['Work_Sans'] pt-5 pb-9">
+          © 2022. Created by Diana P.
+        </h2>
       </section>
     </>
   );
